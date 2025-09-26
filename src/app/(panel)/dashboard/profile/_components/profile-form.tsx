0f324124ae523ff34b004875a601ddbad21af73d
AlvaroPGomes <alvaropgomes@gmail.com>
2025-09-25 23:30:57 -0300

    secao3#aula26 wip criação do form

---
+++ src/app/(panel)/dashboard/profile/_components/profile-form.tsx
@@ -0,0 +1,27 @@
+import {z} from "zod"
+import {zodResolver} from "@hookform/resolvers/zod"
+import {useForm} from "react-hook-form"
+import {Button} from "@/components/ui/button"
+
+const profileSchema = z.object({
+    name: z.string().min(2, {message: "Nome deve ter no mínimo 2 caracteres"}).max(100, {message: "Nome deve ter no máximo 100 caracteres"}),
+    adsress: z.string().optional() ,
+    phone: z.string().optional(),
+    status: z.string,
+    timeZone: z.string().min(2, {message: "Time zone deve ter no mínimo 2 caracteres"}),
+})
+
+type ProfileFormData = z.infer<typeof profileSchema>;
+
+export function userProfileForm() {
+    return useForm<ProfileFormData>({
+        resolver: zodResolver(profileSchema),
+        defaultValues: {
+            name: "",
+            adsress: "",
+            phone: "",
+            status: "Ativo",
+            timeZone: "",
+        }
+    })
+}
