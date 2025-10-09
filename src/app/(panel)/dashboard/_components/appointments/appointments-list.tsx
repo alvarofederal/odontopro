f83b1daab8b64cee587ae76b89f7fa2909ebd635
AlvaroPGomes <alvaropgomes@gmail.com>
2025-10-09 08:53:51 -0300

    listando agendamentos da clínica

--- src/app/(panel)/dashboard/_components/appointments/appointments-list.tsx
+++ src/app/(panel)/dashboard/_components/appointments/appointments-list.tsx
@@ -1,6 +1,8 @@
 "use client"
 
 import { useRouter, useSearchParams } from "next/navigation"
+import { ScrollArea } from '@/components/ui/scroll-area'
+import { Card, CardContent, CardTitle, CardHeader } from "@/components/ui/card"
 
 interface AppointmentsListProps {
     times: string[]
@@ -11,14 +13,40 @@ export function AppointmentsList({ times }: AppointmentsListProps) {
     const searchParams = useSearchParams()
     const date = searchParams.get("date") || ""
 
-    console.log( date )
+    
     
 
 
 
     return (
-        <div>
-            <h2>Appointments List</h2>
-        </div>
+        <Card >
+            <CardHeader className="flex flex-row items-center justify-between space-y-4 pb-4">
+                <CardTitle className="text-xl md:text-2xl font-bold">
+                    Agedamentos
+                </CardTitle>    
+                <button>SELECIONAR DATA</button>
+            </CardHeader>
+            <CardContent>
+                <ScrollArea className="h-[calc(100vh-20rem)] lg:h-[calc(100vh-15rem)] pr-4">
+                    <div className="space-y-2">
+                        {times.length === 0 && (
+                            <p className="text-sm text-gray-500">Nenhum agendamento encontrado.</p> 
+                        )}
+                        {times.map((slot) => {
+                            return (
+                                <div key={slot} className="flex items-center py-2 border-t last:border-b">
+                                    <div key={slot} className="w-16 text-sm font-semibold">  
+                                        {slot} 
+                                    </div>
+                                    <div>
+                                        Disponível
+                                    </div>
+                                </div>                            
+                            )
+                        })}
+                    </div>
+                </ScrollArea>
+            </CardContent>
+        </Card>
     )
 }
