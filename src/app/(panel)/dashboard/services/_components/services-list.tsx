5e84b88c9b1c31b4b02dbc7e5012bbb4f8ada15f
AlvaroPGomes <alvaropgomes@gmail.com>
2025-10-08 22:16:10 -0300

    secao6#aula72 correção do fechamento do dialog no serviço

--- src/app/(panel)/dashboard/services/_components/services-list.tsx
+++ src/app/(panel)/dashboard/services/_components/services-list.tsx
@@ -60,7 +60,13 @@ export function ServicesList({ services }: ServicesListProps) {
   }
 
   return (
-    <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+    <Dialog open={isDialogOpen} onOpenChange={
+      (open) => {
+        setIsDialogOpen(open);
+        if (!open) {
+          setEditingService(null);
+        } 
+      }}>
       <section className='mx-auto'>
         <Card>
           <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
