b69c6c33abb9ececf11e2147c641e847567ac752
AlvaroPGomes <alvaropgomes@gmail.com>
2025-10-11 22:19:16 -0300

    secao6#aula84 carregando clinicas na home

--- src/app/(public)/_components/professionals.tsx
+++ src/app/(public)/_components/professionals.tsx
@@ -1,18 +1,18 @@
 import {
   Card,
   CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
 } from "@/components/ui/card"
 import Image from "next/image"
 import fotoImg from '../../../../public/foto1.png'
 import Link from "next/link"
 import { ArrowRight } from "lucide-react"
+import { User } from "@/generated/prisma"
 
+interface ProfessionalsProps {
+  professionals: User[];
+}
 
-export function Professionals() {
+export function Professionals({professionals}: ProfessionalsProps) {
   return (
     <section className="bg-gray-50 py-16">
 
@@ -22,55 +22,48 @@ export function Professionals() {
         </h2>
 
         <section
-          className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4"
-        >
-
-          <Card className="overflow-hidden">
-            <CardContent className="p-0">
-              <div>
-                <div className="relative h-48">
-                  <Image
-                    src={fotoImg}
-                    alt="Foto da clinica"
-                    fill
-                    className="object-cover"
-                  />
+          className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
+          {professionals.map((clinic) => (
+            <Card className="overflow-hidden hover:shadow-lg duration-300" key={clinic.id}>
+              <CardContent className="p-0">
+                <div>
+                  <div className="relative h-48">
+                    <Image
+                      src={clinic.image ?? fotoImg}
+                      alt="Foto da clinica"
+                      fill
+                      className="object-cover"
+                    />
+                  </div>
                 </div>
-              </div>
 
-              <div className="p-4 space-y-4">
-                <div className="flex items-center justify-between">
-                  <div>
-                    <h3 className="font-semibold">
-                      Clinica centro
-                    </h3>
-                    <p className="text-sm text-gray-500">
-                      Rua x, centro, Campo Grande - MS
-                    </p>
+                <div className="p-4 space-y-4">
+                  <div className="flex items-center justify-between">
+                    <div>
+                      <h3 className="font-semibold">
+                        {clinic.name}
+                      </h3>
+                      <p className="text-sm text-gray-500">
+                        {clinic.adress}
+                      </p>
+                    </div>
+
+                    <div className="w-2.5 h-2.5 rounded-full bg-emerald-500"></div>
                   </div>
 
-                  <div className="w-2.5 h-2.5 rounded-full bg-emerald-500"></div>
+                  <Link
+                    href={`/clinica/${clinic.id}`}
+                    target="_blank"
+                    className="w-full bg-emerald-500 hover:bg-emerald-400 text-white flex items-center justify-center py-2 rounded-md text-sm md:text-base font-medium">
+                    Agendar horário
+                    <ArrowRight className="ml-2" />
+                  </Link>
                 </div>
-
-                <Link
-                  href="/clinica/123"
-                  className="w-full bg-emerald-500 hover:bg-emerald-400 text-white flex items-center justify-center py-2 rounded-md text-sm md:text-base font-medium"
-                >
-                  Agendar horário
-                  <ArrowRight className="ml-2" />
-                </Link>
-              </div>
-            </CardContent>
-          </Card>
-
-
-
-
+              </CardContent>
+            </Card> 
+          ))}
         </section>
-
-
       </div>
-
     </section>
   )
 } 
