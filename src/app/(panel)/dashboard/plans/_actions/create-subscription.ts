5fe4f4c5cd2670ac433988739ecd6ed406d97b9b
AlvaroPGomes <alvaropgomes@gmail.com>
2025-10-13 12:08:55 -0300

    secao7#aula94 (IMPORTANTE) criação do acesso dos produtos a venda no stripe

--- src/app/(panel)/dashboard/plans/_actions/create-subscription.ts
+++ src/app/(panel)/dashboard/plans/_actions/create-subscription.ts
@@ -2,6 +2,8 @@
 
 import { Plan } from '@/generated/prisma';
 import { auth } from '@/lib/auth';
+import prisma from '@/lib/prisma';
+import { stripe } from '@/utils/stripe';
 
 interface SubscriptionProps {
     type: Plan;
@@ -13,6 +15,74 @@ export async function createSubscription({type}: SubscriptionProps) {
     const userId = session?.user?.id;
 
     if(!userId) {
+        return {
+            sessionId: "",
+            error: "Falha ao ativar plano."
+        }
+    }
+    
+    const findUser = await prisma.user.findFirst({
+        where: {
+            id: userId
+        }
+    });
+
+    if(!findUser) {
+        return {
+            sessionId: "",
+            error: "Falha ao ativar plano."
+        }
+    }
+
+    let customerId = findUser.stripeCustomerId;
+
+    if(!customerId) {
+
+        const stripeCostumer = await stripe.customers.create({
+            email: findUser.email || undefined,
+        });
+
+        await prisma.user.update({
+            where: {
+                id: userId
+            },
+            data: {
+                stripeCustomerId: stripeCostumer.id
+            }
+        });
+
+        customerId = stripeCostumer.id;
+    }
+
+    //CRIA CHECKOUT
+    try {
+        
+        const stripeCheckoutSession = await stripe.checkout.sessions.create({
+            customer: customerId,
+            payment_method_types: ["card"],
+            billing_address_collection: "required",
+            line_items: [
+                { price: type === "BASIC" ? process.env.STRIPE_PLAN_BASIC : process.env.STRIPE_PLAN_PRO,
+                  quantity: 1,
+                },
+            ],
+            metadata: { 
+                type: type
+            },
+            mode: 'subscription',
+            allow_promotion_codes: true,
+            success_url: process.env.STRIPE_SUCCESS_URL,
+            cancel_url: process.env.STRIPE_CANCEL_URL,
+        })
+
+        return {
+            sessionId: stripeCheckoutSession.id,
+            url: stripeCheckoutSession.url,
+        }
+
+    } catch (error) {
+        console.log("ERROR AO CRIAR CHECKOUT")
+        console.log(error);
         return {
             sessionId: "",
             error: "Falha ao ativar plano."
--- src/app/(panel)/dashboard/plans/_components/subscription-button.tsx
+++ src/app/(panel)/dashboard/plans/_components/subscription-button.tsx
@@ -3,6 +3,8 @@
 import { Button } from '@/components/ui/button'
 import { Plan } from '@/generated/prisma';
 import { createSubscription } from '../_actions/create-subscription';
+import { toast } from 'sonner';
+import { getStripeJs } from '@/utils/stripe-js';
 
 interface SubscriptionButtonProps {
     type: Plan;
@@ -11,7 +13,19 @@ interface SubscriptionButtonProps {
 export function SubscriptionButton({ type }: SubscriptionButtonProps) {
 
     async function handleCreateBillingPortal() {
-        const response = await createSubscription({type: type});
+        const {sessionId, error, url} = await createSubscription({type: type});
+
+        if(error) {
+            toast.error(error);
+            return;
+        }
+
+        const stripe = await getStripeJs();
+
+        if(stripe && url) {
+            window.location.href = url;
+        }
+
     }
 
     return (
